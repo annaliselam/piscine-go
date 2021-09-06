4ae941b5f7f59e89eb272181699ae443657d5800
fiddlecat <annaliselam1@gmail.com>
2021-09-06 15:11:03 +0100

    My second commit

---
+++ to-git-or-not-to-git.sh
@@ -0,0 +1,1 @@
+curl -s https://learn.01founders.co/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"fiddlecat\"}}){id}}"}' | cut -d "{" -f4 | cut -d "}" -f1 | cut -d ":" -f2
