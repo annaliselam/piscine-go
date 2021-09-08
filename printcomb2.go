9de30121929100d352d13b361a14f5b386458783
fiddlecat <annaliselam1@gmail.com>
2021-09-08 16:51:59 +0100

    mmm

--- printcomb2.go
+++ printcomb2.go
@@ -9,8 +9,7 @@ func PrintComb2() {
 		for b := 48; b <= 57; b++ {
 			for c := 48; c <= 57; c++ {
 				for d := 49; d <= 57; d++ {
-					if c > a || d > b {
-						if c == a {
+					if A != B && C != D && C != A && B != D {
 							// numbers rune
 							z01.PrintRune(rune(a))
 							z01.PrintRune(rune(b))
