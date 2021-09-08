e90141dc0d43506a60b2ba504c95f3ff6fabdb8e
fiddlecat <annaliselam1@gmail.com>
2021-09-08 15:30:43 +0100

    Mmm

--- printcomb2.go
+++ printcomb2.go
@@ -7,10 +7,18 @@ import (
 func PrintCombTwo() {
 	for a := 48; a <= 57; a++ {
 		for b := 48; b <= 57; b++ {
+			for c := 48; c <= 57; c++ {
+				for d := 48; d <= 57; d++ {
 					// numbers rune
 					z01.PrintRune(rune(a))
 					z01.PrintRune(rune(b))
-					if a == 57 && b == 57 {
+					// space rune
+					z01.PrintRune(32)
+					// numbers rune
+					z01.PrintRune(rune(c))
+					z01.PrintRune(rune(d))
+
+					if a == 57 && b == 57 && c == 57 && d == 57 {
 						// comma rune
 						z01.PrintRune(rune(44))
 						// space rune
@@ -19,6 +27,7 @@ func PrintCombTwo() {
 						z01.PrintRune('\n')
 					}
 				}
+			}
 		}
 	}
 }
