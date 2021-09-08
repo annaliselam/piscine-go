2eed0a23e43f49039d3dfadf12eb4e42af4d4acb
fiddlecat <annaliselam1@gmail.com>
2021-09-08 20:56:10 +0100

    mm

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,8 +1,8 @@
 package piscine
 
 func UltimateDivMod(a *int, b *int) {
-	division := a / b
-	modulo := a % b
-	&a = *division
-	&b = *modulo
+	division := *a / *b
+	modulo := *a % *b
+	*a = division
+	*b = division
 }
