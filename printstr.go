60fc7d2372552bbcac9030e7fe7cf1142b166bf0
Annalise Lam <annaliselam1@gmail.com>
2021-09-09 19:00:04 +0100

    mmm

--- printstr.go
+++ printstr.go
@@ -3,7 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	for i rune := range s {
-		z01.PrintRune(i)
-}
+	var s string = " "
+		z01.PrintRune(rune(s[0]))
+		z01.PrintRune(rune(s[1]))
+		z01.PrintRune(rune(s[2]))
+		z01.PrintRune(rune(s[3]))
+		z01.PrintRune(rune(s[4]))
+		z01.PrintRune(rune(s[5]))
+		z01.PrintRune(rune(s[6]))
+		z01.PrintRune(rune(s[7]))
+		z01.PrintRune(rune(s[8]))
+		z01.PrintRune(rune(s[9]))
+		z01.PrintRune(rune(s[10]))
+		
 }
