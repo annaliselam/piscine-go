1b68303d0412da780edef2152ff8d8de4dd5a2d3
fiddlecat <annaliselam1@gmail.com>
2021-09-09 12:17:43 +0100

    mmm

--- printstr.go
+++ printstr.go
@@ -3,14 +3,18 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
+	r := []rune(s)
+	z01.PrintRune(rune(s))
+
+	z01.PrintRune('\n')
+
+	/*
 	slice := []string{
 		"s",
 	}
 
-	for s := range slice {
+	for s := range slice { */
 
-		z01.PrintRune(rune(s))
-
-		z01.PrintRune('\n')
+	
 	}
 }
