378e277f2c92d341ccdb39f9cd8d8e4b83d2ee3f
Annalise Lam <annaliselam1@gmail.com>
2021-09-09 17:41:51 +0100

    mmm

--- printstr.go
+++ printstr.go
@@ -3,16 +3,5 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	str := " "
-
-	slice := []string{
-		str,
-	}
-
-	for s := range slice {
-		z01.PrintRune(rune(s))
-
-		z01.PrintRune('\n')
-
-	}
+	
 }
