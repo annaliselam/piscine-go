8f01593c81b42a7024fd1f2132986b829d6dd6c7
fiddlecat <annaliselam1@gmail.com>
2021-09-10 09:51:21 +0100

    mmm

--- strlen.go
+++ strlen.go
@@ -1,16 +1,9 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func StrLen(s string) int {
 	// Write a function that counts the runes of a string and that returns that count.
 
-	aStringChangeable := []byte(s)
+	length := len(s)
+	return length
 
-	length := len(aStringChangeable)
-	z01.PrintRune(rune(length))
-
-	/* for i := 0; i < len(aStringChangeable); i++ {
-		z01.PrintRune(rune(aStringChangeable[i]))
-	} */
 }
