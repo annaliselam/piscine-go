2ba92a1c5fb1a8e0f9880a10c188badbcaf67028
fiddlecat <annaliselam1@gmail.com>
2021-09-13 17:01:35 +0100

    mm

--- iterativepower.go
+++ iterativepower.go
@@ -8,7 +8,7 @@ func IterativePower(nb int, power int) int {
 	} else if power == 0 {
 		result = 1
 	} else {
-		for i := power; i > 0 ; i-- {
+		for i := power; i > 0; i-- {
 			result *= nb
 			power -= 1
 		}
