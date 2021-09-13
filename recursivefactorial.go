fb6dbebed833b2683a853d3adbcdb093b9a2ec33
fiddlecat <annaliselam1@gmail.com>
2021-09-13 16:15:13 +0100

    mmm

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,10 +1,17 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb >= 1 && nb < 30 {
+	if nb < 0 && nb > 30 {
+		return 0
+	}
+
+	if nb == 0 {
+		return 1
+	}
+
+	if nb >= 1{
 		nb = nb * RecursiveFactorial(nb-1)
-	} else {
-		nb = 0
+	
 	}
 	return nb
 }
