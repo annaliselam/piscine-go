e892928839820701cb8126c3ed038beba3dae563
fiddlecat <annaliselam1@gmail.com>
2021-09-14 19:15:04 +0100

    mmm

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,7 @@ func IsAlpha(s string) bool {
 	runeString := []rune(s)
 
 	for _, letters := range runeString {
-		if (letters < 48) || (letters > 47 && letters < 65) || (letters > 90 && letters < 97) || (letters > 122) {
+		if (letters < 48) || (letters > 57 && letters < 65) || (letters > 90 && letters < 97) || (letters > 122) {
 			return false
 		}
 		if (letters > 47 && letters < 58) || (letters > 64 && letters < 91) || (letters > 96 && letters < 123) {
