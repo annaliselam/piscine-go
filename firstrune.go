499163f0c7452ac7702e20dde3ff484aed7c3ecf
fiddlecat <annaliselam1@gmail.com>
2021-09-14 11:44:17 +0100

    Mmm

--- firstrune.go
+++ firstrune.go
@@ -3,7 +3,8 @@ package piscine
 // Write a function that returns the first rune of a string.
 
 func FirstRune(s string) rune {
-	firstChar := s[0]
+	runeString := []rune(s)
+	firstChar := runeString[0]
 
 	return rune(firstChar)
 }
