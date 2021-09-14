adc562755359e6c71f1c2b42afd0fc15caf51742
fiddlecat <annaliselam1@gmail.com>
2021-09-14 19:12:50 +0100

    mmm

---
+++ isalpha.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	runeString := []rune(s)
+
+	for _, letters := range runeString {
+		if (letters < 48) || (letters > 47 && letters < 65) || (letters > 90 && letters < 97) || (letters > 122) {
+			return false
+		}
+		if (letters > 47 && letters < 58) || (letters > 64 && letters < 91) || (letters > 96 && letters < 123) {
+		}
+
+	}
+	return true
+}
