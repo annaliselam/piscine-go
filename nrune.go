2e34946f9738dce7bc86c457cce2b524ea44d9a7
fiddlecat <annaliselam1@gmail.com>
2021-09-14 12:52:03 +0100

    mmm

--- nrune.go
+++ nrune.go
@@ -3,15 +3,14 @@ package piscine
 func NRune(s string, n int) rune {
 	if n < 0 {
 		return 0
-	} 
-	
+	}
+
 	if n > len(s) {
 		return 0
 	}
-	
+
 	runeString := []rune(s)
 	nChar := runeString[n-1]
-
 
 	return rune(nChar)
 }
--- test/test.go
+++ test/test.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-	z01.PrintRune(piscine.FirstRune("Hello!"))
-	z01.PrintRune(piscine.FirstRune("Salut!"))
-	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune(piscine.NRune("Hello!", 3))
+	z01.PrintRune(piscine.NRune("Salut!", 2))
+	z01.PrintRune(piscine.NRune("Bye!", -1))
+	z01.PrintRune(piscine.NRune("Bye!", 5))
+	z01.PrintRune(piscine.NRune("Ola!", 4))
 	z01.PrintRune('\n')
 }
