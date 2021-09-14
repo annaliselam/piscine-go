bc8aa7a7f03184f5cfebfb16b93b46eabf5d224e
fiddlecat <annaliselam1@gmail.com>
2021-09-14 16:18:28 +0100

    mmm

---
+++ alphacount.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AlphaCount(s string) int {
+	var counter int
+
+	runeString := []rune(s)
+
+	// if i < 65 && i < 90 && i < 97 && i < 122 {
+	for _, letters := range runeString {
+		if (letters > 65 && letters < 90) || ( letters > 97 && letters < 122 ) {
+			counter++
+		}
+	}
+	return counter
+}
