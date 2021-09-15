208d477b7f51eb50fb2594fe3c497cf3d5527ef4
Annalise Lam <annaliselam1@gmail.com>
2021-09-15 08:53:59 +0100

    mmm

--- toupper.go
+++ toupper.go
@@ -1,10 +1,9 @@
 package piscine
 
 func ToUpper(s string) string {
-
 	runeString := []rune(s)
 	var result string = 1
-	
+
 	for _, letters := range runeString {
 		if letters > 96 || letters < 123 {
 			ch := string(letters - 32)
@@ -15,6 +14,3 @@ func ToUpper(s string) string {
 	}
 	return result
 }
-
-
-
