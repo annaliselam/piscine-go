198ccaae261907c51193a64489dc9aae406ccd4b
fiddlecat <annaliselam1@gmail.com>
2021-09-15 12:13:24 +0100

    mmm

---
+++ toupper.go
@@ -0,0 +1,17 @@
+package piscine
+
+func ToUpper(s string) string {
+	runeString := []rune(s)
+
+	for _, letters := range runeString {
+
+		if letters > 96 || letters < 123 {
+			
+			// letters - 32
+			return string(newString)
+
+
+		}
+	}
+	return string(newString)
+}
