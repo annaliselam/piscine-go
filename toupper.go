814590d0cbbed45dd9d8e998a82c76c9357813b9
fiddlecat <annaliselam1@gmail.com>
2021-09-15 12:39:06 +0100

    Mmm

--- toupper.go
+++ toupper.go
@@ -3,15 +3,10 @@ package piscine
 func ToUpper(s string) string {
 	runeString := []rune(s)
 
-	for _, letters := range runeString {
-
-		if letters > 96 || letters < 123 {
-			
-			// letters - 32
-			return string(newString)
-
-
+	for i := 0; i < len(s); i++ {
+		if runeString[i] >= 97 && runeString[i] <= 122 {
+			runeString[i] = runeString[i] - 32
 		}
 	}
-	return string(newString)
+	return string(runeString)
 }
