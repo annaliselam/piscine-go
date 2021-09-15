6cc3f7d882fc59d5dd60b92fdaad7953acd8dd99
Annalise Lam <annaliselam1@gmail.com>
2021-09-15 09:06:22 +0100

    mmm

---
+++ isupper.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsUpper(s string) bool {
+	runeString := []rune(s)
+
+	for _, letters := range runeString {
+		if letters > 97 || letters < 122 {
+			return false
+		}
+		if letters < 96 && letters > 123 {
+		}
+
+	}
+	return true
+}
