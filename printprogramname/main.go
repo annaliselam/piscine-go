07079dbb5721a8a916aacd2255375858850c2c9b
fiddlecat <annaliselam1@gmail.com>
2021-09-16 11:47:45 +0100

    mmm

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,9 +1,19 @@
 package main
 
+// Write a program that prints the name of the program
+
 import (
 	"os"
+	"github.com/01-edu/z01"
 )
 
-func main() {
-	os.Args[0]
+func main() { 
+	arguments := os.Args
+	argName := len(arguments)
+
+	for i := 1; i < len(arguments); i++ {
+		z01.PrintRune(rune(argName))
+	}
+	
+	z01.PrintRune('\n')
 }
