36d037ae26aa14c97e3a9cd14e496d2088ff1043
fiddlecat <annaliselam1@gmail.com>
2021-09-16 11:50:22 +0100

    mmm

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -4,16 +4,16 @@ package main
 
 import (
 	"os"
+
 	"github.com/01-edu/z01"
 )
 
-func main() { 
+func main() {
 	arguments := os.Args
 	argName := len(arguments)
 
 	for i := 1; i < len(arguments); i++ {
 		z01.PrintRune(rune(argName))
 	}
-	
 	z01.PrintRune('\n')
 }
