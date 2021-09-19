c1221b985cefa32b2b4b147c1d3ac5dfce198bd2
fiddlecat <annaliselam1@gmail.com>
2021-09-19 17:43:44 +0100

    mmm

---
+++ makerange.go
@@ -0,0 +1,22 @@
+package piscine
+
+func MakeRange(min, max int) []int {	
+	var size int
+
+	if min > max {
+		size = 0
+	} else {
+		size = max - min
+	}
+
+	answer := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		answer[i] = (max-min)+1
+		for i := range answer {
+			answer[i] = min + i
+		}
+	}
+
+	return answer
+}
