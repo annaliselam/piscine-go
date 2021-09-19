9d663aca3383c0d314ccb60cdea976c56a39e48c
fiddlecat <annaliselam1@gmail.com>
2021-09-19 17:48:43 +0100

    mmm

--- makerange.go
+++ makerange.go
@@ -3,7 +3,9 @@ package piscine
 func MakeRange(min, max int) []int {
 	var size int
 
-	if min > max {
+	if min >= max {
+		size = 0
+	} else if min < 0 || max < 0 {
 		size = 0
 	} else {
 		size = max - min
