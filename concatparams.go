49b3d0d56f322ae2df3e572071590adbf1ee508a
fiddlecat <annaliselam1@gmail.com>
2021-09-19 19:23:52 +0100

    mmm

--- concatparams.go
+++ concatparams.go
@@ -1,12 +1,12 @@
 package piscine
 
-// Write a function that takes the arguments received in parameters and returns them as a string. 
+// Write a function that takes the arguments received in parameters and returns them as a string.
 // The string is the result of all the arguments concatenated with a newline (\n) between.
 
 func ConcatParams(args []string) string {
 	// answer := make([]int, len(args))
 	answerString := ""
-	
+
 	for i := 0; i < len(args); i++ {
 		for _, x := range args[i] {
 			answerString += string(x)
@@ -14,5 +14,4 @@ func ConcatParams(args []string) string {
 	}
 
 	return answerString
-
 }
