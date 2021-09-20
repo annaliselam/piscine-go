5e78a6c113efbafe5e81feea56b37877e3a49097
Annalise Lam <annaliselam1@gmail.com>
2021-09-20 10:05:26 +0100

    mmm

---
+++ boolean/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+// use os.args to count the number of arguments
+// check the number of arguments, then return an error message 
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(numArgs int) bool {
+	if (numArgs % 2) == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	var numArgs int
+
+	for i := 1; i < len(args); i++ {
+		numArgs += i 
+	}
+
+	if isEven(numArgs){
+		printStr("I have an even number of arguments")
+	} else {
+		printStr("I have an odd number of arguments")
+	}
+}
