ab7a61a69d21347b37cf8950dec03b78fb0ea7ef
Annalise Lam <annaliselam1@gmail.com>
2021-09-20 12:27:35 +0100

    create displayfile/

---
+++ displayfile/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"fmt"
+//	"io/ioutil"
+)
+
+func main() {
+	multipleArgs := len(os.Args)
+	if multipleArgs > 2 {
+		fmt.Println("Too many arguments")
+		return
+	} else if multipleArgs < 2 {
+		fmt.Println("File name missing")
+		return
+	}
+
+	file, err := os.Open("quest8.txt")
+
+	arr := make([]byte, 14)
+	file.Read(arr)
+
+	fmt.Println(string(arr))
+
+	file.Close()
+	
+}
+
