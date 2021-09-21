63f94fbf13ef5c2e06db2fa39458a806cccd99a2
fiddlecat <annaliselam1@gmail.com>
2021-09-21 13:44:54 +0100

    mmm

---
+++ any.go
@@ -0,0 +1,13 @@
+package piscine
+
+// Write a function Any that returns true, for a string slice :
+// if, when that string slice is passed through an f function, at least one element returns true.
+
+func Any(f func(string) bool, a []string) bool {
+	for _, value := range a {
+		if f(value) {
+			return true
+		}
+	}
+	return false
+}
