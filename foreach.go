78881ea9c4a2d08b17142340f591f1f2be76da1e
fiddlecat <annaliselam1@gmail.com>
2021-09-21 13:03:51 +0100

    mmm

--- foreach.go
+++ foreach.go
@@ -3,7 +3,6 @@ package piscine
 // Write a function ForEach that, for an int slice, applies a function on each elements of that slice.
 
 func ForEach(f func(int), a []int) {
-
 	for _, b := range a {
 		a = f(b)
 	}
